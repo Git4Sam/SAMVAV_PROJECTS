3ccad012b4d487d51c6716c62ecf5c6a761dfb38
SAMVAV <162554445+Git4Sam@users.noreply.github.com>
2024-06-30 20:16:12 +0200

    Add files via upload
    Randomly select a winning name &/or number from a list. You can edit the items of the names and numbers list.

---
+++ Random_Name&Number_Picker.py
@@ -0,0 +1,21 @@
+import random
+
+names = ["Jane ", "Jade ", "James "]
+
+numbers = [10, 20, 30]
+
+winning_name = random.choice(names)
+winning_number = random.choice(numbers)
+
+
+print(winning_name)
+print(winning_number)
+
+
+
+
+
+
+
+
+
